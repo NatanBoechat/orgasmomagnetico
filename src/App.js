c6ae7f7a1cc1060301e7f8140a4dee85e48df6e4
NatanBoechat <91276639+NatanBoechat@users.noreply.github.com>
2021-09-23 12:51:48 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+import Mouth from './img/icone.png'
+
+function App() {
+  const [brasil, setBrasil] = useState(false);
+
+  function redirecionar() {
+    if (brasil) {
+      window.location.href = 'https://www.google.com';
+    } else {
+      window.location.href = '';
+    }
+  }
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('https://ipinfo.io/?token=9c8a933f1acc15');
+      const data = await response.json();
+      if (data.country === 'BR') {
+        setBrasil(true);
+      }
+    }
+    getData();
+  }, []);
+
+  return (
+    <div className="mainContent">
+      <img src={ Mouth } alt="Mouth Icon" className="mouth"/>
+      <h1 className="headline">
+        Você está prestes a viver uma experiência sem limites
+      </h1>
+
+      <button className="btnEmbarcar" onClick={ redirecionar }>
+        Embarcar
+      </button>
+    </div>
+  );
+}
+
+export default App;
