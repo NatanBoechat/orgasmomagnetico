0761d8f66483dfc3073f552f497c46166b2e2c04
NatanBoechat <91276639+NatanBoechat@users.noreply.github.com>
2021-10-09 12:17:47 -0300

    Update index.js

--- src/Pages/Initial/index.js
+++ src/Pages/Initial/index.js
@@ -3,6 +3,25 @@ import './App.css';
 
 import Mouth from '../../img/icone.png'
 
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '900723827493782');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=900723827493782&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+
 function Initial() {
   const [brasil, setBrasil] = useState(false);
 
